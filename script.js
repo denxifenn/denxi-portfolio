dcaecb855a8862126da7633b767abcf26cdfc6ba
denxifenn <144514992+denxifenn@users.noreply.github.com>
2025-06-19 17:20:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+{/* <script>
+  const faders = document.querySelectorAll('.fade-in');
+
+  const appearOnScroll = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+</script> */}
